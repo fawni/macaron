bdac54e56053e8b9544b73b137bc34dfa8788aef
fawn <fawn@rinici.de>
2023-12-12 01:57:45 +0200

    🚧 wip: init

---
+++ README.md
@@ -0,0 +1,12 @@
+# macaron🍩
+
+minimalistic toast notifications
+
+## todo
+
+- [ ] show multiple macarons at once (container)
+- [ ] simple config for the things that can't be customized with css
+
+## license
+
+[Apache-2.0](LICENSE)
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+enum Style {
+    Info,
+    Success,
+    Error,
+    Warn,
+}
+
+const Classes = {
+    [Style.Info]: "macaron-info",
+    [Style.Success]: "macaron-success",
+    [Style.Error]: "macaron-error",
+    [Style.Warn]: "macaron-warn"
+}
+
+function info(message: string) {
+    show(message, Style.Info)
+}
+
+function success(message: string) {
+    show(message, Style.Success)
+}
+
+function error(message: string) {
+    show(message, Style.Error)
+}
+
+function warn(message: string) {
+    show(message, Style.Warn)
+}
+
+function show(message: string, style: Style) {
+    const macaron = document.createElement("div");
+    macaron.innerText = message;
+    macaron.classList.add("macaron", Classes[style])
+    document.body.appendChild(macaron);
+    setTimeout(() => macaron.remove(), 2 * 1000);
+}
